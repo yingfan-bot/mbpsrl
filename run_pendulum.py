ffba2cd3d954bbe5032cc6cbbd8566f62e5f46e0
eunicefying@gmail.com <eunicefying@gmail.com>
2021-10-23 19:05:37 -0500

    new change training iter

--- run_pendulum.py
+++ run_pendulum.py
@@ -69,11 +69,11 @@ if __name__ == '__main__':
     action_shape = len([env.action_space.sample()])
 
     dx_model = construct_shallow_model(obs_dim=obs_shape, act_dim=action_shape, hidden_dim=200, num_networks=1, num_elites=1)
-    cost_model = construct_shallow_cost_model(obs_dim=obs_shape, act_dim=action_shape, hidden_dim=10, num_networks=1, num_elites=1)
+    cost_model = construct_shallow_cost_model(obs_dim=obs_shape, act_dim=action_shape, hidden_dim=200, num_networks=1, num_elites=1)
 
-    my_dx = neural_bays_dx_tf(args, dx_model, "dx", obs_shape, sigma_n2=0.001**2,sigma2= 1**2)
+    my_dx = neural_bays_dx_tf(args, dx_model, "dx", obs_shape, sigma_n2=0.01**2,sigma2= 0.1**2)
 
-    my_cost = neural_bays_dx_tf(args, cost_model, "cost", 1, sigma_n2 = 0.001**2,sigma2 = 1**2)
+    my_cost = neural_bays_dx_tf(args, cost_model, "cost", 1, sigma_n2 = 0.01**2,sigma2 = 0.1**2)
 
 
 
@@ -147,9 +147,9 @@ if __name__ == '__main__':
         rewards.append([episode, cum_rewards.tolist()[0]])
 
 
-        my_dx.train(epochs = 100)
+        my_dx.train(epochs = 200)
         my_dx.update_bays_reg()
-        my_cost.train(epochs = 100)
+        my_cost.train(epochs = 200)
         my_cost.update_bays_reg()
         print(rewards)
 
