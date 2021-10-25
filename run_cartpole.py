77fce24ebd0938b4646fe007d6708cebec79da93
eunicefying@gmail.com <eunicefying@gmail.com>
2021-10-24 22:10:59 -0500

    new change sigma cartpole

--- run_cartpole.py
+++ run_cartpole.py
@@ -78,9 +78,9 @@ if __name__ == '__main__':
     dx_model = construct_shallow_model(obs_dim=obs_shape, act_dim=action_shape, hidden_dim=200, num_networks=1, num_elites=1)
     cost_model = construct_shallow_cost_model(obs_dim=obs_shape, act_dim=action_shape, hidden_dim=200, num_networks=1, num_elites=1)
 
-    my_dx = neural_bays_dx_tf(args, dx_model, "dx", obs_shape, sigma2=0.1**2, sigma_n2=0.0001**2)
+    my_dx = neural_bays_dx_tf(args, dx_model, "dx", obs_shape, sigma2=1**2, sigma_n2=0.001**2)
 
-    my_cost = neural_bays_dx_tf(args, cost_model, "cost", 1, sigma2=0.1**2, sigma_n2=0.0001**2)
+    my_cost = neural_bays_dx_tf(args, cost_model, "cost", 1, sigma2=1**2, sigma_n2=0.001**2)
 
 
     avg_loss = []
